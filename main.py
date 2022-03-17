11b23e6e895f8a34a4c6c3ea67eacf13e5ecd12a
micfun123 <54761507+micfun123@users.noreply.github.com>
2022-03-17 19:27:57 +0400

    pls work

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-from fastapi.responses import StreamingResponse
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse , RedirectResponse , StreamingResponse
+from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
 from minecraft import MCtext
 
-app = FastAPI()
+app = FastAPI(
+    title = "Mics TextAPI",
+    description="Generate image text in a video game font \n This what made by Michael twitter = https://twitter.com/Michaelrbparker ",
+    version="0.1.0",
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+
+)
+
 
 @app.get("/")
 def root():
